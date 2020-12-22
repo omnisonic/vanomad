00001a2002e48ef07c2bacb506b03987582f9c77
johnclarkemusic <omnisonic@gmail.com>
2020-12-21 20:04:03 -0800

    abolute urls testing in publish config

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,7 +31,7 @@ AUTHOR_FEED_RSS = None
 DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+RELATIVE_URLS = False
 
 IMAGE_PROCESS = {
     "article-image": ["scale_in 300 300 True"],
